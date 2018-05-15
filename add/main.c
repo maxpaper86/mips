a1257309fd49d9c1dad853af8f9fe0f9420b99c9
Jangwon Han <jangwon_han@tmax.co.kr>
2018-05-15 16:09:31 +0900

    remove unused folder

--- add/main.c
+++ add/main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
-int main(){
-	int number[10]={1,2,3,4,5,6,7,8,9,10};
-	number[4] = 9
-		number[5]=number[1]+number[2]
-		printf("%d %d",number[4],number[5]);
-	return 0;
+int number[10]={1,2,3,4,5,6,7,8,9,10};
+
+int main(void){
+    number[4] = 9
+    number[5]=number[1]+number[2]
+    printf("%d %d",number[4],number[5]);
+
+    return 0;
 }
