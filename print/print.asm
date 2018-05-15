730367b88e96e36de7e177908564b5bc0eaf55fc
Jangwon Han <jangwon_han@tmax.co.kr>
2018-05-15 16:52:08 +0900

    add more comment

--- print/print.asm
+++ print/print.asm
@@ -9,7 +9,7 @@ var_int:        .word 0
 main:
 ## var_char = '1'
                 li $t0, '1'             # $t0 = '1'
-                sb $t0, var_char        # var_char = '1'
+                sb $t0, var_char        # var_char = $t0
 ## printf("%c\n", var_char);
                 li $v0, 11              # syscall 11 (print_character)
                 xor $a0, $a0, $a0       # $a0 = 0
@@ -19,8 +19,8 @@ main:
                 li $a0, '\n'            # $a0 = '\n'
                 syscall                 # issue syscall 11
 ## var_short = 2
-                li $t0, 2
-                sh $t0, var_short
+                li $t0, 2               # $t0 = 2
+                sh $t0, var_short       # var_short = $t0
 ## printf("%d\n", var_short);
                 li $v0, 1               # syscall 1 (print_int)
                 xor $a0, $a0, $a0       # $a0 = 0
@@ -30,8 +30,8 @@ main:
                 li $a0, '\n'            # $a0 = '\n'
                 syscall                 # issue syscall 11
 ## var_int = 4
-                li $t0, 4
-                sw $t0, var_int
+                li $t0, 4               # $t0 = 4
+                sw $t0, var_int         # var_int = $t0
 ## printf("%d\n", var_int);
                 li $v0, 1               # syscall 1 (print_int)
                 xor $a0, $a0, $a0       # $a0 = 0
