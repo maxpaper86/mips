8aead508980e61926fd31fa46de68a4fc3fd6320
Jangwon Han <jangwon_han@tmax.co.kr>
2018-05-15 15:55:16 +0900

    add C version of print program

---
+++ print/print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char var_char = '1';
+short var_short = 2;
+char var_int = 4;
+
+int main(void)
+{
+    printf("%c\n", var_char);
+    printf("%d\n", var_short);
+    printf("%d\n", var_int);
+    return 0;
+}
